2362f3e3ada94e49654934b4982cd2a2b6898452
Ivan Shcherbak <alotofall@gmail.com>
2018-11-14 15:25:43 +0200

    init application

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = async app => {
+    app.on('issues.opened', async context => {
+        const params = context.issue({
+            body: 'Hello World!'
+        });
+        await context.github.issues.createComment(params)
+    })
+};
