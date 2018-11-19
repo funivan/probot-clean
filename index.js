910d4ed3a080cdcc8dc9340366846690823c8a2f
Ivan Shcherbak <alotofall@gmail.com>
2018-11-19 19:47:57 +0200

    Prototype issue creating

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const createScheduler = require('probot-scheduler')
 module.exports = async app => {
-  app.on('issues.opened', async context => {
-    const params = context.issue({
-      body: 'Hello World!'
+  app.log('Probot Clean app is running successfully.')
+  app.on('schedule.repository', async (context) => {
+    app.log(`App is running as per schedule.repository`)
+    app.log('Local Time: ' + new Date())
+    const { owner, repo } = context.repo()
+    app.log(`Repository: ${owner}/${repo}`)
+    await context.github.issues.create({
+      owner,
+      repo,
+      title: 'Waiting for release',
+      body: 'Too long not released. \n Commits after latest release %s \n Outdated for %d days',
+      labels: ['release-me']
     })
-    await context.github.issues.createComment(params)
   })
+  createScheduler(
+    app,
+    { interval: 12 * 60 * 60 * 1000 } // 12 hours
+  )
 }
